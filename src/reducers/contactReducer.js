43d90c13d43c5bc322d5f5dbdbda33a30000ecbe
mdestafadilah <desta.08b@gmail.com>
2020-06-09 11:46:20 +0700

    Added To Redux State

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,4 +1,25 @@
-const initState = {};
+const initState = {
+  contacts: [
+    {
+      id: 1,
+      name: "yasweb",
+      email: "yasweb@gmail.com",
+      phone: "0666666"
+    },
+    {
+      id: 2,
+      name: "yasweb react",
+      email: "react@gmail.com",
+      phone: "07777777777"
+    },
+    {
+      id: 3,
+      name: "yasweb symfony",
+      email: "symfony@gmail.com",
+      phone: "08888888"
+    }
+  ]
+};
 
 export default function(state = initState, action) {
   switch (action.type) {
